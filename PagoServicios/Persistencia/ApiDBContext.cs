4ad970fe9beb89100a1dde6884c128bd6a505e50
Eder <matiasbm002@gmail.com>
2022-12-29 19:44:26 -0300

    Estructura general

---
+++ PagoServicios/Persistencia/ApiDBContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia
+{
+    public class ApiDBContext : DbContext
+    {
+
+        private const string connString = "User ID=local;Password=123456;Host=localhost;Port=9401;Database=Pagos;Pooling=true;Min Pool Size=0;Max Pool Size=100;Connection Lifetime=0;";
+        public ApiDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(connString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
